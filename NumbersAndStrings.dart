bba242c6927d170589987fc78d546728d66c31ce
daniel-zuluaga <daniel.zuluaga007@gmail.com>
2022-08-10 21:59:14 -0500

    first commit

---
+++ NumbersAndStrings.dart
@@ -0,0 +1,31 @@
+void main() {
+    
+  // Strings
+  var nombre = "Tony";
+  var apellido = "Stark";
+  print("$nombre $apellido"); 
+  
+  String nombre2 = "Peter";
+  String apellido2 = "Shark";
+  print("$nombre2 $apellido2");
+  
+  final nombre3 = "Jupiter";
+  final apellido3 = "Hachi";
+  print("$nombre3 $apellido3");
+  
+  const nombre4 = "Fenix";
+  const apellido4 = "Alexander";
+  print("$nombre4 $apellido4");
+  
+  // Numbers And decimal
+  int empleados = 10;
+  
+  double salarios = 1080.25;
+  
+  print(empleados);
+  print(salarios);
+  
+  
+}
+
+
